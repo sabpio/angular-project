ff51ce3f3c3670e8d57b28a3304efc8980d666b2
sabpio <sabacinski.piotr@gmail.com>
2016-04-12 21:20:27 +0200

    first commit

---
+++ src/main/java/com/example/ItemController.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+	@Autowired
+	private ItemRepository itemRepository;
+
+	@RequestMapping(method = RequestMethod.POST)
+	public Item addItem(@RequestBody Item item) {
+		item.setId(null);
+		return itemRepository.saveAndFlush(item);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public Item updateItem(@RequestBody Item updatedItem,
+			@PathVariable Integer id) {
+		updatedItem.setId(id);
+		return itemRepository.saveAndFlush(updatedItem);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deleteItem(@PathVariable Integer id) {
+		itemRepository.delete(id);
+	}
+}
